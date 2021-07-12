5a0fe83189fdad5ddb980828324a6a5fbc18d21d
Humberto Navarro <humberto.navarro.dev@gmail.com>
2021-07-12 16:55:17 -0700

    Listen for 'submit' events on the journal entry form and record data

--- js/main.js
+++ js/main.js
@@ -2,33 +2,19 @@
 /* exported data */
 var entries = [];
 var nextEntryId = 0;
-var entryID
 var $image = document.querySelector('.form-image');
 var $photoUrl = document.querySelector('.photo-url');
 var $form = document.querySelector('form');
-
-function handleURLChange(event) {
-  if(checkURL($photoUrl.value)){
-    $image.src = $photoUrl.value;
+if(localStorage.getItem('entries') !== null) {
+ entries = JSON.parse(localStorage.getItem('entries'));
+ var biggest = entries[0].entryId;
+ for(var i = 0; i < entries.length; i++) {
+  if(entries[i].entryId > biggest) {
+    biggest = entries[i].entryId;
   }
+ }
+ nextEntryId = biggest;
 }
-function handleFormSubmit(event) {
-  event.preventDefault();
-  var entry = {
-    title: $form.title.value,
-    url: $form.url.value,
-    notes: $form.notes.value,
-    entryID: nextEntryId
-  };
-  entries.push(entry);
-  $form.title.value = '';
-  $form.url.value = '';
-  $form.notes.value = '';
-  $image.src = 'images/placeholder-image-square.jpg'
-  entryID++;
-}
-$photoUrl.addEventListener('input',handleURLChange);
-$form.addEventListener('submit', handleFormSubmit);
 
 function checkURL(url) {
   if(!url.startsWith('https://')){
@@ -36,3 +22,28 @@ function checkURL(url) {
   }
   return(url.match(/\.(jpeg|jpg|gif|png)$/) != null);
 }
+
+function handleURLChange(event) {
+  if(checkURL($photoUrl.value)){
+    $image.src = $photoUrl.value;
+  }
+}
+function handleFormSubmit(event) {
+  nextEntryId++;
+  event.preventDefault();
+  var entry = {
+    title: $form.title.value,
+    url: $form.url.value,
+    notes: $form.notes.value,
+    entryId: nextEntryId
+  };
+  entries.push(entry);
+  $form.title.value = '';
+  $form.url.value = '';
+  $form.notes.value = '';
+  $image.src = 'images/placeholder-image-square.jpg';
+  var entriesString = JSON.stringify(entries);
+  localStorage.setItem('entries',entriesString);
+}
+$photoUrl.addEventListener('input',handleURLChange);
+$form.addEventListener('submit', handleFormSubmit);
